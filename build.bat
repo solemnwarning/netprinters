4a706c5b7c97384e9c6d5e18ac3cba5806ae3389
Daniel Collins <solemnwarning@solemnwarning.net>
2008-02-25 00:08:39 +0000

    Re-licensed as BSD and fixed the broken build system

--- build.bat
+++
@@ -1,5 +0,0 @@
-rem Build netprinters.exe
-rem Must be executed from the VS command shell
-
-C:\MinGW\bin\gcc.exe -Wall -c -o netprinters.obj netprinters.c
-CL stub.c netprinters.obj /link winspool.lib ws2_32.lib /out:netprinters.exe
--- netprinters.c
+++ netprinters.c
@@ -1,8 +1,31 @@
-/* Setup network printers
- * By Daniel Collins <solemnwarning@solemnwarning.net>
- * Released to public domain
+/* NetPrinters - Source
+ * Copyright (C) 2008 Daniel Collins <solemnwarning@solemnwarning.net>
+ * All rights reserved.
  *
- * This must be linked with winspool.lib, ws2_32.lib and stub.c
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *	* Redistributions of source code must retain the above copyright
+ *	  notice, this list of conditions and the following disclaimer.
+ *
+ *	* Redistributions in binary form must reproduce the above copyright
+ *	  notice, this list of conditions and the following disclaimer in the
+ *	  documentation and/or other materials provided with the distribution.
+ *
+ *	* Neither the name of the author nor the names of its contributors may
+ *	  be used to endorse or promote products derived from this software
+ *	  without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include <windows.h>
@@ -75,7 +98,7 @@ int ncasecmp(char const* a, char const* b) {
 	return 1;
 }
 
-int rmain(int argc, char** argv) {
+int main(int argc, char** argv) {
 	if(argc < 2) {
 		EPRINTF("Usage: %s <printer list>\n", argv[0]);
 		return 1;
@@ -147,7 +170,7 @@ int rmain(int argc, char** argv) {
 		if(ncasecmp(command, "default")) {
 			printf("Setting default:\t%s\n", printer);
 			
-			if(!set_printer(printer)) {
+			if(!SetDefaultPrinter(printer)) {
 				int errnum = GetLastError();
 				EPRINTF("Can't set default printer: %s\n", strerror(errnum));
 			}
